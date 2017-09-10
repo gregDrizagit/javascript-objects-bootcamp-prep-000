597190f4be366e81fe2e4b0a5e99904549aa83c3
Greg Driza <gdriza@uarts.edu>
2017-09-10 13:45:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = [];
 function updatePlaylist(playlist, artistName, songTitle)
 {
   var songObj = {artistName, songTitle};
-  return playlist.unshift(); = {artistName:songTitle};
+  return playlist.unshift(songObj);
 }
 
 function removeFromPlaylist(playlist, artistName)
